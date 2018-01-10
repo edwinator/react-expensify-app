93b4d93a712fac33d6567fae58c7d51182e3d9cf
Edwin Veltman <edwinator@gmail.com>
2018-01-10 11:33:39 -0800

    setup production webpack build

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Git Commands
+
+.gitignore file
+  which directories ignored by git
+
+command lines:
+
+git init 
+  create a new git repo
+
+git status
+  view the changes to your project code
+
+git add src/file.js
+  add files to the staging area
+
+git commit -m "message"
+  create a new commit with staged files
+
+git log
+  view recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,48 @@
-// entry point -> output bundle file
-// see webpack.js.org
-
+// using Node so use 'require' rather than 'import'
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-// entry: './src/app.js',
-    // // entry: './src/playground/redux-101.js',
-    // entry: './src/playground/destructuring.js',
-    // entry: './src/playground/redux-expensify.js',
-    // entry: './src/playground/hoc.js',
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [ 
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production'; 
+    const CSSExtract = new ExtractTextPlugin('styles.css');   
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap:true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap:true
+                            }                            
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }    
     }
-
 };
